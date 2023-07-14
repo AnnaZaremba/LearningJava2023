ca4c83a4c532a13c856b084d3511b4551a8accff
Anna Zaremba <aniazarem@gmail.com>
2023-07-14 09:40:40 +0200

    Spring Boot

--- src/main/java/pl/annaz/LearningJava/LearningJavaApplication.java
+++ src/main/java/pl/annaz/LearningJava/LearningJavaApplication.java
@@ -5,9 +5,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class LearningJavaApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(LearningJavaApplication.class, args);
-	}
+    /*
+    Java od podstaw
+    */
+    public static void main(String[] args) {
+        SpringApplication.run(LearningJavaApplication.class, args);
+    }
 
 }
